e53b96c628ea87b098b65c800ac828b22bb3604a
rakafajars <rakafajar18@gmail.com>
2021-01-31 05:21:44 +0700

    membuat table user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,8 +20,18 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+
             $table->foreignId('current_team_id')->nullable();
             $table->text('profile_photo_path')->nullable();
+
+            $table->text('address')->default();
+            $table->string('houserNumber')->nullable();
+            $table->string('phoneNumber')->nullable();
+            $table->string('city')->nullable();
+
+            $table->string('roles')->default('USER');
+
+
             $table->timestamps();
         });
     }
