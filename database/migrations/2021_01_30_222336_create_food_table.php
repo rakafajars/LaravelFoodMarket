6e134d8eb7f375f228055e73021474443034ba44
rakafajars <rakafajar18@gmail.com>
2021-01-31 05:28:27 +0700

    Migration food table

---
+++ database/migrations/2021_01_30_222336_create_food_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFoodTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('food', function (Blueprint $table) {
+            $table->id();
+            $table->string('nameFood')->nullable();
+            $table->text('description')->nullable();
+            $table->text('ingredients')->nullable();
+            $table->integer('priceFood')->nullable();
+            $table->double('rateFood')->nullable();
+            $table->string('typeFood')->nullable();
+            $table->text('picturePath')->nullable();
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('food');
+    }
+}
